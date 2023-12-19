cdcce8f0c9a0d294bdd0804f09253ce9a20508ec
LadyDi3103 <chinamy2004@gmail.com>
2023-12-19 04:10:13 -0500

    UPDATE: BACKEND

--- src/index.js
+++ src/index.js
@@ -28,6 +28,7 @@ app.get('/', (req, res) => {
   res.send('¡Hola, mundo!');
 });
 
+// Obtener todos los médicos
 app.get('/medicos', (req, res) => {
     // Realizar una consulta a la base de datos
     connection.query('SELECT * FROM medicos', (error, results, fields) => {
@@ -35,7 +36,58 @@ app.get('/medicos', (req, res) => {
       res.json(results); // Enviar los resultados como respuesta JSON
     });
   });
+
+  // Obtener todos los pacientes
+app.get('/pacientes', (req, res) => {
+  connection.query('SELECT * FROM pacientes', (error, results, fields) => {
+    if (error) throw error;
+    res.json(results);
+  });
+});
   
+// Obtener un paciente por ID
+app.get('/pacientes/:id', (req, res) => {
+  const pacienteId = req.params.id;
+  connection.query('SELECT * FROM pacientes WHERE id = ?', [pacienteId], (error, results, fields) => {
+    if (error) throw error;
+    if (results.length > 0) {
+      res.json(results[0]);
+    } else {
+      res.status(404).send('Paciente no encontrado');
+    }
+  });
+});
+
+// Crear un nuevo paciente
+app.post('/pacientes', (req, res) => {
+  const nuevoPaciente = req.body; // Asumiendo que los datos del paciente se envían en el cuerpo de la solicitud
+  connection.query('INSERT INTO MAE_pacientes SET ?', nuevoPaciente, (error, results, fields) => {
+    if (error) throw error;
+    res.json({ id: results.insertId, ...nuevoPaciente });
+  });
+});
+
+// Actualizar un paciente por ID
+app.put('/pacientes/:id', (req, res) => {
+  const pacienteId = req.params.id;
+  const datosActualizados = req.body; // Asumiendo que los datos actualizados se envían en el cuerpo de la solicitud
+  connection.query('UPDATE pacientes SET ? WHERE id = ?', [datosActualizados, pacienteId], (error, results, fields) => {
+    if (error) throw error;
+    res.json({ id: pacienteId, ...datosActualizados });
+  });
+});
+
+// Eliminar un paciente por ID
+app.delete('/pacientes/:id', (req, res) => {
+  const pacienteId = req.params.id;
+  connection.query('DELETE FROM pacientes WHERE id = ?', [pacienteId], (error, results, fields) => {
+    if (error) throw error;
+    res.send('Paciente eliminado correctamente');
+  });
+});
+
+
+
 
 app.listen(PORT, () => {
   console.log(`Servidor corriendo en http://localhost:${PORT}`);
